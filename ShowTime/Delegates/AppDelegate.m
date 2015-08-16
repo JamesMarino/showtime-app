d93ec99c0fd0e9ac423493f68107184f998e6654
James Marino <jamesmarinoau@gmail.com>
2015-08-16 10:45:48 +1000

    Initial App - Cleaned Up

---
+++ ShowTime/Delegates/AppDelegate.m
@@ -0,0 +1,21 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
+	
+}
+
+@end
---
+++ ShowTime/Support/main.m
@@ -0,0 +1,7 @@
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	// Entry Point
+	return NSApplicationMain(argc, argv);
+}
